4640f9e382a2d55fe0f446e743b1ca17187db7e1
Niko Pinnis <niko.pinnis@gmail.com>
2017-08-12 18:37:26 +0300

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+var Main = require('Main');
+
+// load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// app css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main} />
+  </Router>,
+  document.getElementById('app')
+);
